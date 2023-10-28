51ae1e778755474b91a96c80b99abacd23573810
Eno LEROY <eno.leroy@gmail.com>
2023-10-28 17:34:48 +0200

    config de la navbar et creation des pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
+
+  get 'about_me', to: 'pages#about_me'
+  get 'projects', to: 'pages#projects'
 end
